0d80dd62cce543b64d7d9024c8d434b155ef8e6c
mrmilul <miladmizani@yahoo.com>
2021-10-06 19:58:02 +0300

    primary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
 
-function App() {
+
+// -----------------------Css file--------------------------------
+import './App.css'
+
+
+// -----------------------Redux Store--------------------------------
+import store from './redux';
+
+
+// -----------------------Components--------------------------------
+import Layout from './components/layout/Layout'
+import Home from './components/Home';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Layout>
+            <Route exact path="/" component={Home} />
+          </Layout>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            This is home component
+        </div>
+    )
+}
+
+export default Home
